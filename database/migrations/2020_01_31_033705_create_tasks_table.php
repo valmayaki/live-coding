7679a08344fc26d6516d3a4999abf900018e9003
Valentine Mayaki <mxvmayaki@gmail.com>
2020-01-31 05:47:47 +0100

    inital task migration

---
+++ database/migrations/2020_01_31_033705_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->unsignedBigInteger('user_id');
+            $table->text("description");
+            $table->timestamp("due_at")->nullable();
+            $table->timestamp("done_at")->nullable();
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table');
+    }
+}
